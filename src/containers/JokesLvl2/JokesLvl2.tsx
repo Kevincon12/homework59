e0c25ab2d07e2f0ff5d9ffa7147edec373b04524
Strelnikov Ilia <ily_strelnikov@mail.ru>
2025-12-18 20:04:12 +0600

    Добавили use Effect, функцию запроса, и базовую верстку

--- src/containers/JokesLvl2/JokesLvl2.tsx
+++ src/containers/JokesLvl2/JokesLvl2.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const JokesLvl2 = () => {
+    const [jokeString, setJokeString]= useState('');
+    const URL: string = 'https://api.chucknorris.io/jokes/random';
+
+    const makeJoke = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        const joke = data.value;
+        console.log(joke);
+    }
+
+    useEffect(() => {
+        makeJoke();
+    }, []);
+
     return (
         <div>
-            
+            <p>If you wanna laugh click the button</p>
+            <button className='btn btn-primary'>Get joke</button>
         </div>
     );
 };
